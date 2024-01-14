801e8969165edec994e6aed77dce9bf8c4ee750e
Andrea Marangione <marangione9@gmail.com>
2024-01-15 00:19:16 +0100

    M2W4D1 done

---
+++ M2W4D1/js/script.js
@@ -0,0 +1,57 @@
+function findJob(title, location) {
+    const jobsFound = {
+        result: [],
+        count: 0,
+    }
+    jobs.forEach(job => {
+        if (job.title.toLowerCase().includes(title.toLowerCase()) && job.location.toLocaleLowerCase().includes(location.toLocaleLowerCase())) {
+            jobsFound.result.push(job);
+        }
+    })
+    jobsFound.count = jobsFound.result.length;
+    return jobsFound;
+}
+
+function deleteJobList() {
+    const body = document.querySelector("body");
+    const jobListContainer = document.querySelector(".jobListContainer");
+    if(body.contains(jobListContainer)) {
+        jobListContainer.remove();
+    }
+}
+
+function ReadFormAndSearchJob() {
+    const jobTitle = document.querySelector("#jobTitle");
+    const jobLocation = document.querySelector("#jobLocation");
+    if (jobTitle.value.trim(" ") && jobLocation.value.trim(" ")) {
+        const jobsFound = findJob(jobTitle.value, jobLocation.value);
+        jobTitle.value = "";
+        jobLocation.value = "";
+        return jobsFound;
+    }
+}
+
+function createJobList(jobsFound) {
+    if (jobsFound != undefined) {
+        const body = document.querySelector("body");
+        const jobListContainer = document.createElement("div");
+        const list = document.createElement("ul");
+        jobListContainer.classList.add("jobListContainer");
+        list.classList.add("list");
+        jobsFound.result.forEach(jobResult => {
+            const job = document.createElement("li");
+            job.classList.add("job");
+            job.innerHTML = "<p><strong>TITLE:</strong> " + jobResult.title + "</p> <p><strong>LOCATION:</strong> "  + jobResult.location + "</p>";
+            list.append(job);
+        })
+        jobListContainer.append(list);
+        body.append(jobListContainer);
+    }
+}
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    deleteJobList();
+    createJobList(ReadFormAndSearchJob());
+})
