4575ccb35a2c83e1cf21dfd3f79b9b1f48d1615e
Andrea Marangione <marangione9@gmail.com>
2024-01-15 19:57:03 +0100

    BW1 first realease

--- M2W4D1/js/script.js
+++ M2W4D1/js/script.js
@@ -15,7 +15,7 @@ function findJob(title, location) {
 function deleteJobList() {
     const body = document.querySelector("body");
     const jobListContainer = document.querySelector(".jobListContainer");
-    if(body.contains(jobListContainer)) {
+    if (body.contains(jobListContainer)) {
         jobListContainer.remove();
     }
 }
@@ -23,28 +23,29 @@ function deleteJobList() {
 function ReadFormAndSearchJob() {
     const jobTitle = document.querySelector("#jobTitle");
     const jobLocation = document.querySelector("#jobLocation");
-    if (jobTitle.value.trim(" ") && jobLocation.value.trim(" ")) {
-        const jobsFound = findJob(jobTitle.value, jobLocation.value);
-        jobTitle.value = "";
-        jobLocation.value = "";
-        return jobsFound;
-    }
+    const jobsFound = findJob(jobTitle.value, jobLocation.value);
+    jobTitle.value = "";
+    jobLocation.value = "";
+    return jobsFound;
 }
 
 function createJobList(jobsFound) {
     if (jobsFound != undefined) {
         const body = document.querySelector("body");
         const jobListContainer = document.createElement("div");
+        const resultsCounter = document.createElement("p");
         const list = document.createElement("ul");
         jobListContainer.classList.add("jobListContainer");
+        resultsCounter.classList.add("jobListResults");
         list.classList.add("list");
+        resultsCounter.textContent = `Results: ${jobsFound.count}`;
         jobsFound.result.forEach(jobResult => {
             const job = document.createElement("li");
             job.classList.add("job");
-            job.innerHTML = "<p><strong>TITLE:</strong> " + jobResult.title + "</p> <p><strong>LOCATION:</strong> "  + jobResult.location + "</p>";
+            job.innerHTML = "<p><strong>TITLE:</strong> " + jobResult.title + "</p> <p><strong>LOCATION:</strong> " + jobResult.location + "</p>";
             list.append(job);
         })
-        jobListContainer.append(list);
+        jobListContainer.append(resultsCounter, list);
         body.append(jobListContainer);
     }
 }
