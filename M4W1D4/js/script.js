1531bb10b278fd0c302dacc358635b6fd8a7fc0c
Andrea Marangione <marangione9@gmail.com>
2024-03-11 15:26:39 +0100

    M4W1D4 done

---
+++ M4W1D4/js/script.js
@@ -0,0 +1,98 @@
+const alborosie = document.querySelector("#alborosieSection");
+const eminem = document.querySelector("#eminemSection");
+const metallica = document.querySelector("#metallicaSection");
+const queen = document.querySelector("#queenSection");
+const modalBtn = document.querySelector("#modalBtn");
+
+const createAlbums = (song, artist) => {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h5");
+    div.id = "cardAlbum";
+    div.classList.add("col", "col-3");
+    img.classList.add("w-100");
+    img.src = song.album.cover_xl;
+    title.innerText = song.title;
+    div.append(img, title);
+    artist.append(div);
+}
+
+const getAlborosieAlbums = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=alborosie");
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const alborosieAlbums = getAlborosieAlbums().then(res => {
+    for (let index = 0; index < 4; index++) {
+        createAlbums(res[index], alborosie);
+    }
+})
+
+const getEminemAlbums = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem");
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const eminemAlbums = getEminemAlbums().then(res => {
+    for (let index = 0; index < 4; index++) {
+        createAlbums(res[index], eminem);
+    }
+})
+
+const getMetallicaAlbums = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica");
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const metallicaAlbums = getMetallicaAlbums().then(res => {
+    for (let index = 0; index < 4; index++) {
+        createAlbums(res[index], metallica);
+    }
+})
+
+const getQueenAlbums = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen");
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const queenAlbums = getQueenAlbums().then(res => {
+    for (let index = 0; index < 4; index++) {
+        createAlbums(res[index], queen);
+    }
+})
+
+modalBtn.addEventListener("click", () => {
+    const songList = document.querySelector("#songList");
+    const cardAlbumTitle = document.querySelectorAll("#cardAlbum > h5");
+    const list = document.createElement("ul");
+    if (songList.childElementCount > 0) {
+        const oldList = document.querySelector("ul");
+        songList.removeChild(oldList);
+    }
+    cardAlbumTitle.forEach(title => {
+        const song = document.createElement("li");
+        song.innerHTML = `<p>${title.innerText}</p>`;
+        list.append(song);
+    })
+    songList.append(list);
+})
